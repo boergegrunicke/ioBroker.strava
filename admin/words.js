4a391963958b01e649c25985bf3e029063cdfd9d
ioBroker Adapter Creator <noreply@iobroker.net>
2023-11-10 10:22:24 +0000

    Initial commit by adapter creator

---
+++ admin/words.js
@@ -0,0 +1,43 @@
+/* eslint no-unused-vars: off */
+/* eslint no-global-assign: off */
+/* global systemDictionary */
+'use strict';
+
+systemDictionary = {
+	'strava adapter settings': {
+		en: 'Adapter settings for strava',
+		de: 'Adaptereinstellungen für strava',
+		ru: 'Настройки адаптера для strava',
+		pt: 'Configurações do adaptador para strava',
+		nl: 'Adapterinstellingen voor strava',
+		fr: "Paramètres d'adaptateur pour strava",
+		it: "Impostazioni dell'adattatore per strava",
+		es: 'Ajustes del adaptador para strava',
+		pl: 'Ustawienia adaptera dla strava',
+		'zh-cn': 'strava的适配器设置',
+	},
+	clientId: {
+		en: 'Client ID',
+		de: 'Client ID',
+		ru: 'Client ID',
+		pt: 'Client ID',
+		nl: 'Client ID',
+		fr: 'Client ID',
+		it: 'Client ID',
+		es: 'Client ID',
+		pl: 'Client ID',
+		'zh-cn': 'Client ID',
+	},
+	clientSecret: {
+		en: 'Client Secret',
+		de: 'Client Secret',
+		ru: 'Client Secret',
+		pt: 'Client Secret',
+		nl: 'Client Secret',
+		fr: 'Client Secret',
+		it: 'Client Secret',
+		es: 'Client Secret',
+		pl: 'Client Secret',
+		'zh-cn': 'Client Secret',
+	},
+};
